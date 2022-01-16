9162dd18e70fd84e28a49576dc09e954df7147bb
Roman Borodavkin <barad1tos@gmail.com>
2022-01-16 22:59:08 +0200

    2nd variant of terraform modules

---
+++ 01-terraform-modules/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.70.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+/*
+
+provider "cloudflare" {
+  email = var.cloudflare_email
+}
+*/
+
